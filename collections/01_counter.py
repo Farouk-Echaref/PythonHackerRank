e04bf154dcc1399b277973fbf32ade1ee1cb6782
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2024-11-09 17:12:11 +0100

    collections

---
+++ collections/01_counter.py
@@ -0,0 +1,11 @@
+from collections import Counter
+from typing import List, Tuple
+
+# def returnCounter(arr: List) -> Counter:
+#     return Counter(arr)
+
+# if __name__ == "__main__":
+#     myList : List = [1,1,2,3,4,5,3,2,3,4,2,1,2,3]
+#     print(returnCounter(myList))
+#     print(returnCounter(myList).items())
+#     print(returnCounter(myList).keys())
