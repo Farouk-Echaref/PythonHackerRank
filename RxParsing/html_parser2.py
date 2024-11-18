00e15dbd1324dccc27425c1bb1ffc0ef803a4c10
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2024-11-18 18:42:13 +0100

    html parser 2

--- RxParsing/html_parser2.py
+++ RxParsing/html_parser2.py
@@ -2,3 +2,25 @@
 
 from html.parser import HTMLParser
 
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data):
+        if "\n" in data:
+            print(">>> Multi-line Comment")
+        else:
+            print(">>> Single-line Comment")
+        print(data)
+    def handle_data(self, data):
+        if data.strip():
+            print(">>> Data")
+            print(data)
+
+if __name__ == "__main__":
+    html_str = ""
+    my_parser = MyHTMLParser()
+    N = int(input())
+    while N:
+        html_str += input()
+        html_str += '\n'
+        N -= 1
+    my_parser.feed(html_str)
+    my_parser.close()
