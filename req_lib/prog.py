2d4cdaf03fbf558d9bfa57aa34cba8d8a89380d6
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2024-11-19 18:45:04 +0100

    request lib: basics

--- req_lib/prog.py
+++ req_lib/prog.py
@@ -8,16 +8,46 @@ res = requests.get("https://api.github.com")
 # check if the req was successful (res < 400 (200 and 399))
 if res:
     if res.status_code == 200:
-        content = res.content
+        content : bytes = res.content
         json = res.json
         print(json)
+elif res.status_code == 403:
+    print("Rate limit")
 elif res.status_code == 404:
     print("Not Found.")
     
 
-URLS = ["https://api.github.com", "https://api.github.com/invalid"]
+URLS = ["https://api.github.com", "https://realpython.com/python-requests/", "https://api.github.com/invalid"]
 
 for url in URLS:
-    response = requests.get(url)
-    response.raise_for_status()
-except 
+    try:
+        response = requests.get(url)
+        # info about metadata for ex: HEADERS
+        headers = response.headers
+        # print(headers)
+        # dict lookup (case insensitive) : headers["content-type"]
+        print(headers["Content-Type"])
+        # output: 'application/json; charset=utf-8'
+        paylod_bytes : bytes = response.content
+        # enforce encoding
+        # response.encoding = "utf-8"
+        paylod_str : str = response.text
+        print("----------------- Serialized JSON:")
+        print(paylod_str)
+        print()
+        # can use json.loads() to deserialize it
+        print("----------------- Dictionary format: (Deserialized)")
+        payload_dict = response.json()
+        print(payload_dict)
+        
+        # can access values like with key/val:
+        emo_url : str = payload_dict["emojis_url"]
+        # output from the payload: 'https://api.github.com/emojis'
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f"HTTP error occured: {http_err}")
+    except Exception as err:
+        print(f"Other error occured: {err}")
+    else:
+        print("Success.")
+    break
--- req_lib/rd.md
+++ req_lib/rd.md
@@ -1,1 +1,4 @@
 https://realpython.com/python-requests/
+
+* JSON is a format that encodes objects in a string. Serialization means to convert an object into that string, and deserialization is its inverse operation (convert string -> object).
+
